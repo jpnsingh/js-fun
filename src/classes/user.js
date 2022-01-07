5279c60dcfa0efa75ff7c2f57e438ed55f1dd790
jpnsingh <jpnsingh86@gmail.com>
2022-01-08 02:13:10 +0530

    Work with classes;  - Explore class features;

--- src/classes/user.js
+++ src/classes/user.js
@@ -14,4 +14,24 @@
 
   let user = new User('JPN Singh');
   user.sayHi();
+
+  // class is a function
+  console.log(typeof User); // function
+
+  // ...or, more precisely, the constructor method
+  console.log(User === User.prototype.constructor); // true
+
+  // The methods are in User.prototype, e.g:
+  console.log(User.prototype.sayHi); // the code of the sayHi method
+
+  // there are exactly two methods in the prototype
+  console.log(Object.getOwnPropertyNames(User.prototype)); // constructor, sayHi
+
+  // class methods are not enumerable by default
+  // so they won't appear if we do a for..in on objects
+  for (var key in user) {
+    if (user.hasOwnProperty(key)) {
+      console.log(key);
+    }
+  }
 })('Working with classes');
