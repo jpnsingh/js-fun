22af3b1ce29c0f542157b20d0eca369f361f3e05
jpnsingh <jpnsingh86@gmail.com>
2022-01-17 00:33:50 +0530

    Work with Promise;  - Simple promise object and promise executor;

---
+++ src/promises/promise-executor.js
@@ -0,0 +1,34 @@
+(function(info){
+  console.log(info);
+
+  let promise = new Promise(function(resolve, reject) {
+    console.log('In promise executor');
+
+    setTimeout(() => resolve("done"), 1000);
+  });
+
+  promise.then((resolve) => console.log(resolve));
+})('Promise | resolve');
+
+(function(info){
+  console.log(info);
+
+  let promise = new Promise(function(resolve, reject) {
+    console.log('In promise executor');
+
+    setTimeout(() => reject(new Error("Whoops!")), 1000);
+  });
+
+  promise.then((resolve) => console.log(resolve));
+})('Promise | reject');
+
+(function(info){
+  console.log(info);
+
+  let promise = new Promise(function(resolve, reject) {
+    console.log('In promise executor');
+    resolve(123);
+  });
+
+  promise.then((resolve) => console.log(resolve));
+})('Promise | Immediate');
